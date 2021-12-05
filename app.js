fc97116b19eb0ac047174365bdb08a3a91d647a8
PedroSerraoViegas <95571125+PedroSerraoViegas@users.noreply.github.com>
2021-12-05 10:42:02 -0300

    Erase and ClearAll feature
    Added the Erase and ClearAll features to the calculator

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const screen = document.querySelector('#calcScreen');
 const display = document.querySelector('#calcDisplay');
+const erase = document.querySelector('#erase');
+const clearAll = document.querySelector('#clearAll');
 const specialButton = document.querySelector('#specialButton');
 const buttonClass = document.getElementsByClassName('inputButton');
 
@@ -12,3 +14,21 @@ for (let i = 0; i< buttonClass.length; i++) {
 specialButton.addEventListener('click', () => {
     display.textContent += specialButton.textContent;
 })
+
+erase.addEventListener('click', () => {
+  let displayedNum = display.textContent;
+  if (display.textContent.length > 1) {
+    let erasedNUM = displayedNum.slice(0, -1)
+    display.textContent = parseFloat(erasedNUM);
+  } else {
+    resetCalc();
+  }
+})
+
+clearAll.addEventListener('click', () => {
+  resetCalc();
+})
+
+function resetCalc() {
+  display.textContent = '';
+}
