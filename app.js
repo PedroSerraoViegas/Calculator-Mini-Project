7eb7bb3fe166a4f34df25981f590cd455ca70f0d
PedroSerraoViegas <95571125+PedroSerraoViegas@users.noreply.github.com>
2021-12-05 11:43:56 -0300

    Adding feature
    Added the 'equals to' and 'add' features and updated some of the 'clear display' and 'clear all' features

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const screen = document.querySelector('#calcScreen');
 const display = document.querySelector('#calcDisplay');
 const erase = document.querySelector('#erase');
+const history = document.querySelector('#history');
+const equalsButton = document.querySelector('#equalsTo');
+const addButton = document.querySelector('#add');
 const clearAll = document.querySelector('#clearAll');
 const specialButton = document.querySelector('#specialButton');
 const buttonClass = document.getElementsByClassName('inputButton');
+let operand1 = '';
+let operand2 = '';
+let result = '';
 
 for (let i = 0; i< buttonClass.length; i++) {
   buttonClass[i].addEventListener('click', () => {
     display.textContent += parseInt(buttonClass[i].textContent);
 })
 }
+
+equalsButton.addEventListener('click', () => {
+  equalsTo();
+})
+
+addButton.addEventListener('click', () => {
+  add();
+})
+
 
 specialButton.addEventListener('click', () => {
     display.textContent += specialButton.textContent;
@@ -31,4 +46,42 @@ clearAll.addEventListener('click', () => {
 
 function resetCalc() {
   display.textContent = '';
+  while (history.firstChild) {
+    history.removeChild(history.firstChild);
+  }
+}
+
+function resetDisplay() {
+  display.textContent = '';
+}
+
+function add() {
+  let operand1 = document.createElement('li');
+  let plusSign = document.createElement('li');
+  plusSign.textContent = '+';
+  let a = parseFloat(display.textContent);
+  operand1.textContent = a;
+  history.appendChild(operand1);
+  history.appendChild(plusSign);
+  resetDisplay();
+}
+
+function equalsTo() {
+  let operand2 = document.createElement('li');
+  let equalsSign = document.createElement('li');
+  equalsSign.textContent = '=';
+  let a = parseFloat(display.textContent);
+  operand2.textContent = a;
+  history.appendChild(operand2);
+  history.appendChild(equalsSign);
+  operate();
+}
+
+function operate() {
+  let operand1 = parseFloat(history.children[0].textContent);
+  let operator = history.children[1].textContent;
+  let operand2 = parseFloat(history.children[2].textContent);
+  let result = eval(operand1 + operator + operand2);
+  resetDisplay();
+  display.textContent = result;
 }
